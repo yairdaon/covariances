39fc9c6f1da353d6d9b1b1d87f5a6a29b98b4ca0
Yair Daon <yair.daon@gmail.com>
2015-08-22 17:55:13 -0400

    just going home...

--- cov/laps.py
+++ cov/laps.py
@@ -3,10 +3,22 @@ import numpy as np
 from pynfft.nfft import NFFT
 from pynfft import Solver
 
+# choose which "test" to run
+test = True
+
+# Points 
+n_dims = 1
+n_grid =900
+n_pts = 50
+N = np.ones( n_dims ) * n_grid
+alpha = 1.0
+k = np.array([ 1 ])
 
 def make_exponential( points, k ):
+    
     factor = -2j * np.pi
-    prod = factor * np.einsum("ij , j -> i", points, k)
+    #prod = factor * np.einsum("ij , j -> i", points, k)
+    prod = factor * points * k 
     ret = np.exp( prod )
     return ret 
 
@@ -32,66 +44,69 @@ def fourier_multiplier(f, eigs, power, plan, infft, eps = 1E-7 ):
     #assert( eigs.shape == infft.f_hat_iter.shape )
 
     # use as a Fourier Multiplier
-    f_hat = infft.f_hat_iter * eigs**power
+    f_hat = infft.f_hat_iter * ( eigs**power )
     
     # transform back
     plan.f_hat = f_hat
     return plan.trafo()
 
-
-def laplacian_eigenvalues( plan, eps = 0.5 ):
+def laplacian_eigenvalues( plan, alpha = 0.5 ):
+    '''
+    These are the eigenvalues of the laplacian
+    they are stored as a grid correspoinding to the 
+    frequencies
+    '''
     dims = plan.N
-    k_x = np.arange( -dims[1] / 2, dims[1] / 2 )
-    k_y = np.arange( -dims[0] / 2, dims[0] / 2 )
-    k_x, k_y = np.meshgrid( k_x, k_y )
-    eigs = 4 * np.pi * np.pi * ( k_x * k_x + k_y * k_y ) + eps
+
+    if hasattr( dims, "size" ) and dims.size == 2:
+        k_x = np.arange( -dims[1] / 2, dims[1] / 2 ) / plan.N[1]
+        k_y = np.arange( -dims[0] / 2, dims[0] / 2 ) / plan.N[0]
+        k_x, k_y = np.meshgrid( k_x, k_y )
+        eigs = 4 * np.pi * np.pi * ( k_x**2 + k_y**2 ) + alpha
+    else:
+        k = np.arange( -dims[0] / 2, dims[0] / 2 ) / plan.N[0]
+        eigs = k**2 + alpha
     return eigs
 
+# initializations...
+points = np.random.uniform( -0.5, 0.5, n_pts * n_dims ) # randome numbers 
+points = points.reshape( n_pts , n_dims ) # reshape to get rand points
+plan  = NFFT( N, points.shape[0] ) # I have a PLAN
+plan.x = points # set the points in the plan
+plan.precompute() # we need to do this for some reason
+infft = Solver( plan ) # solver finds fourier coefficients
+eigs = laplacian_eigenvalues( plan, alpha = alpha ) # eigenvalues of my operator
+f_rand = make_rand_data( points ) # random data
+f_exp = make_exponential( points, k ) # an exponential is an egeinfunction of laplacian     
 
-
-
-
-
-
-
-# Points 
-n_pts = 10
-n_dims = 2
-points = np.random.uniform( -0.5, 0.5, n_pts * n_dims )
-points = points.reshape( n_pts , n_dims )
-
-
-# create solver etc
-n_grid = 500
-plan  = NFFT( [ n_grid, n_grid ], points.shape[0] )
-plan.x = points
-plan.precompute()
-infft = Solver( plan )
-eigs = laplacian_eigenvalues( plan, eps = 1.0 )
-f_rand = make_rand_data( points )
-k = np.array([ -1,1 ])
-f_exp = make_exponential( points, k )     
-
-test = True
-if not test:
+if test:
  
     # Check that we can reconstruct the function
-    f_new = fourier_multiplier( f_rand, eigs, 0, plan, infft, eps = 1E-9 )   
+    f_new = fourier_multiplier( f_rand, eigs, 0, plan, infft, eps = 1E-16 )   
     assert( np.allclose( f_rand, f_new ) )
 
+    # check that laplacian^2 = composition of laplacians
     f1 = fourier_multiplier( f_rand, eigs, 1, plan, infft, eps = 1E-9 )
     f11= fourier_multiplier( f1    , eigs, 1, plan, infft, eps = 1E-9 )
     f2 = fourier_multiplier( f_rand, eigs, 2, plan, infft, eps = 1E-9 )    
     assert( np.allclose( f11, f2 ) )  
 
-if test:    
+
     
+    power = 1 
+
+    # This is the *LAPLACIAN* eigenvalue associated with the exponential
     eigenvalue = np.sum( k * k )
-    f_new = fourier_multiplier( f_exp, eigs, 1, plan, infft, eps = 1E-14 )
+
+    
+    # apply the laplacian to the eigenfunction
+    f_new = fourier_multiplier( f_exp, eigs, power, plan, infft, eps = 1E-34 )
+
+    # this should have eigenvalue + alpha
     ratios = f_new / f_exp
 
-    print( eigenvalue )
-    print( np.mean( ratios ) / n_grid / n_grid )
+    print( "Should be: " + str(         ( eigenvalue + alpha )**power ) )
+    print( "Is:        " + str(  np.mean( ratios             )        ) )
     
     #p = 0.005
     #f_lap_inv = fourier_multiplier( rand_f, eigs, -p, plan, infft, eps = 1E-9 )
@@ -102,8 +117,5 @@ if test:
 
 if False:
     import matplotlib.pyplot as plt
-    plt.plot(interior[:, 0], interior[ :,1], 'd' , color = 'g' )
-    plt.plot(exterior[:, 0], exterior[ :,1], 'd' , color = 'b' )
-    plt.plot(perimeter[:,0], perimeter[:,1], 'o' , color = 'r' )
-    plt.title( "red = bdry, green = interior, blue = extrerior" )
+    plt.plot(points[:,0], points[:,1], 'o' , color = 'r' )
     plt.show()
--- cov/tmp.py
+++ cov/tmp.py
@@ -6,10 +6,10 @@ from pynfft import Solver
 N =1000
 
 # Number of non uniform "nodes"
-M = 100
+M = 700
 
 # The wavenumber of the eigenfunction we consider
-k = 1
+k = -4
 
 def make_eigenfunction( points, k ):
     '''
@@ -38,39 +38,52 @@ def fourier_multiplier(f, eigs, plan, infft, eps = 1E-7 ):
     plan.f_hat = f_hat
     return plan.trafo()
 
-    def laplacian_eigenvalues( plan ):
-        '''
-        These are the eigenvalues of the negative laplacian
-        they are stored as a grid correspoinding to the 
-        wavenumber
-        '''
-        N = plan.N[0]
+def laplacian_eigenvalues( plan ):
+    '''
+    These are the eigenvalues of the negative laplacian
+    they are stored as a grid correspoinding to the 
+    wavenumber
+    '''
+    N = plan.N[0]
         
-        # Seems like I need to divide by N. I have no idea why, though.
-        k = np.arange( -N / 2, N / 2 ) / N 
-        eigs =-(2*np.pi*k)**2
-        return eigs
-    
-    # initializations...
-    points = np.random.uniform( -0.5, 0.5, M) # randome points in the desired range 
-    plan  = NFFT( N, M ) # I have a PLAN
-    plan.x = points # set the points in the plan
-    plan.precompute() # we need to do this for some reason
-    infft = Solver( plan ) # solver finds fourier coefficients
+    # Seems like I need to divide by N. I have no idea why, though.
+    k = np.arange( -N / 2, N / 2 ) / N 
+    eigs =-(2*np.pi*k)**2
+    return eigs
 
-    # eigenvalues of laplacian with fourier basis
-    eigs = laplacian_eigenvalues( plan ) # eigenvalues of my operator
-    
-    f_eig = make_eigenfunction( points, k ) # an exponential is an egeinfunction of laplacian     
+# initializations...
+points = np.random.uniform( -0.5, 0.5, M) # randome points in the desired range 
+plan  = NFFT( N, M ) # I have a PLAN
+plan.x = points # set the points in the plan
+plan.precompute() # we need to do this for some reason
+infft = Solver( plan ) # solver finds fourier coefficients
 
-    # This is the *LAPLACIAN* eigenvalue associated with the exponential
-    eigenvalue = ( 2*np.pi*k ) ** 2
+# eigenvalues of laplacian with fourier basis
+eigs = laplacian_eigenvalues( plan ) # eigenvalues of my 
+f_eig = make_eigenfunction( points, k ) # an exponential is an egeinfunction of laplacian     
+
+# Check that we can reconstruct the function
+f_new = fourier_multiplier( f_eig, eigs**0, plan, infft, eps = 1E-16 )   
+assert( np.allclose( f_eig, f_new ) )
+
+# check that laplacian^2 = composition of laplacians
+f1 = fourier_multiplier( f_eig, eigs**0, plan, infft, eps = 1E-9 )
+f11= fourier_multiplier( f1   , eigs   , plan, infft, eps = 1E-9 )
+f2 = fourier_multiplier( f_eig, eigs**2, plan, infft, eps = 1E-9 ) 
+f1 = fourier_multiplier( f2   , eigs   , plan, infft, eps = 1E-9 )   
+f3 = fourier_multiplier( f_eig, eigs**3, plan, infft, eps = 1E-9 )
+assert( np.allclose( f11, f2  ) )  
+assert( np.allclose( f3 , f21 ) )
+
+# This is the *LAPLACIAN* eigenvalue associated with the eigenfunction
+eigenvalue = -( 2*np.pi*k ) ** 2
+
+# apply the laplacian to the eigenfunction
+f_new = fourier_multiplier( f_eig, eigs1, plan, infft, eps = 1E-6 )
     
-    # apply the laplacian to the eigenfunction
-    f_new = fourier_multiplier( f_eig, eigs, plan, infft, eps = 1E-6 )
+# this should have eigenvalue
+ratios = np.divide( f_new , f_eig )
     
-    # this should have eigenvalue
-    ratios = np.divide( f_new , f_eig )
-    
-    print( "Should be: " + str( eigenvalue ) )
-    print( "Is:\n"       + str( ratios     ) )
+print( "Should be: " + str( eigenvalue ) )
+print( "Is:\n"       + str( ratios     ) )
+assert( np.allclose( f_new , f_eig ) )
