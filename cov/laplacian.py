95c15d6e7590f30c3d5f1c954cd1d40faf477a00
Yair Daon <yair.daon@gmail.com>
2015-09-05 21:44:51 -0400

    weight laplacian vs. identity in a reasonable way

--- cov/laplacian.py
+++ cov/laplacian.py
@@ -4,8 +4,16 @@ from numpy.fft import irfft as irfft
 import matplotlib.pyplot as plt
 import math
 
+# Number of times we call Fourier Multiplier
+count = 0
 
 def dot( u, v ):
+    '''
+    We do a dot product that approximates
+    the L2 (funciton space) dot product.
+    We ignore imaginary parts since all we 
+    do we do in real domains
+    '''
     n = len( u ) 
     zeros = np.zeros( n )
     assert np.allclose( u.imag , zeros )
@@ -13,33 +21,58 @@ def dot( u, v ):
     return np.dot( u.real, v.real ) / n
 
 def norm( u ):
+    '''
+    Approximate the L2 norm in function space!
+
+    '''
     return math.sqrt( dot( u, u ) ) 
 
 def fourier_multiplier( f, eigs ):
     '''
     use an operator that is a Fourier multiplier
     '''
+    # How many times we use this Fourier multiplier?
+    global count
+    count = count + 1
+
+
     assert np.allclose( f.imag , np.zeros ( len( f ) ) )
     f_hat = rfft( f )
     
     assert len( f_hat ) == len( eigs ), str( len( f_hat ) ) + "\n" + str( len ( eigs )  )
     f_hat = f_hat * eigs
         
+    
     return irfft( f_hat )    
 
 def laplacian_eigenvalues( N, alpha ):
     '''
-    These are the eigenvalues of the negative laplacian
-    they are stored as a grid correspoinding to the 
-    wavenumber
+    These are the eigenvalues of the laplacian-like
+    operator. 
+
+    We take a convex combination of the identity operator
+    and the laplacian-like operator. The coefficient of
+    the identity is alpha and the coefficient of the
+    laplacian-like operator is 1-alpha = beta.
     '''
+    # Evenly numbered domains are your friend.
     assert( N % 2 == 0 )
+
+    # Alpha is the relative weight we give the 
+    # identity operator. 
+    assert( alpha > 0 and alpha < 1 )
+
+    # This is the weight we give the laplacian-like
+    # operator.
+    beta = 1 - alpha
+
+    # What we get when twice differnetiating complex exponentials
     factor = 4 * np.pi * np.pi 
 
     eigs = np.arange( 0, N/2 + 1 ) 
-    assert len( eigs ) == N/2 + 1 , str(len(eigs) ) + "\n" + str(N/2+1)
+    assert len( eigs ) == N/2 + 1 , str( len(eigs) ) + "\n" + str( N/2+1 )
     
-    return alpha + eigs * eigs * factor
+    return alpha + factor * eigs * eigs * beta 
 
 
 def make_f( N ):
@@ -47,11 +80,12 @@ def make_f( N ):
     iid N(0,1) data
     '''
     factors = np.arange( 0, N/2 + 1 ) + 1
+    #factors[0:15] = 1
     f_hat_real = np.random.normal( loc = 0.0, scale = 1, size = N/2+1 ) / factors**1.3 
     f_hat_imag = np.random.normal( loc = 0.0, scale = 1, size = N/2+1 ) / factors**1.3
     f = irfft( f_hat_real + 1j * f_hat_imag, N )
     assert len( f ) == N
-    return f
+    return f #/ norm( f )
 
 if __name__ == "__main__":
 
@@ -64,26 +98,33 @@ if __name__ == "__main__":
     points = np.linspace( -0.5,  0.5, N ,endpoint = False )
     
     # eigenvalues of laplacian with fourier basis
-    alpha = 0.1
+    alpha = 0.25
+    beta = 1 - alpha
     eigs = laplacian_eigenvalues( N, alpha ) 
     f = make_f( N )
     eigenvector = f / norm( f )  
     eigenvalue = dot( fourier_multiplier( eigenvector, eigs ), eigenvector )
-    approx = 1
+    wavenumber = math.sqrt(  (eigenvalue-alpha) / beta  ) / np.pi / 2 
+    approx = np.min( np.abs( eigs - eigenvalue ) )
     i = 0
     while approx > 1E-10:  
-        print()
-        print( i )
+        
+        # Plot previous
         plt.plot( points, eigenvector )
         axes = plt.gca()
         axes.set_ylim( [-2, 2 ] )
         axes.set_xlim( [ -0.5,0.5 ] )
-        plt.savefig( "inverse_basix" + str( i ) + ".png")
+        title = "Estimates: $k = %.2f$, Error $= %.4f$" % ( wavenumber, approx )
+        plt.title( title )
+        plt.savefig( "Rayleigh Quotient Iteration " + str( i ) + ".png")
         plt.close()  
     
+        # Update
         inverse_eigenvalues = np.power( eigs - eigenvalue, -1 )
         new = fourier_multiplier( eigenvector, inverse_eigenvalues )
         new = new / norm( new )  
+        
+        # Take care of everything else
         res1 = norm( new - eigenvector ) 
         res2 = norm( new + eigenvector ) 
         if res1 > res2:
@@ -94,15 +135,20 @@ if __name__ == "__main__":
             res = res1
 
         eigenvalue = dot( fourier_multiplier( eigenvector, eigs ), eigenvector )  
-        wavenumber = math.sqrt( eigenvalue - alpha ) / np.pi / 2  
+        wavenumber = math.sqrt( (eigenvalue - alpha)/beta ) / np.pi / 2  
+
         approx = np.min( np.abs( eigs - eigenvalue ) )
-        print( "wavenumber    = " + str( wavenumber ) )
-        print( "e.value error = " + str( approx ) ) 
         i = i + 1
     
-    
-    
-   
+ # Plot Last
+plt.plot( points, eigenvector )
+axes = plt.gca()
+axes.set_ylim( [-2, 2 ] )
+axes.set_xlim( [ -0.5,0.5 ] )
+title = "Estimates: $k = %.2f$, Error $= %.4f$" % ( wavenumber, approx )
+plt.title( title )
+plt.savefig( "Rayleigh Quotient Iteration " + str( i ) + ".png")
+plt.close()   
     
     
     
