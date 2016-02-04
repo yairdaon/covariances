ed6c44c14c83fc976217b198a5fe00cff3cbc917
Yair Daon <yd546@nyu.edu>
2016-02-03 19:09:16 -0500

    finalized

--- 2D/robin.py
+++ 2D/robin.py
@@ -10,74 +10,22 @@ import scipy as sp
 # Choose a mesh
 if True:
     file_name = "lshape.xml"
-    file_name = "dolfin_coarse.xml"
-    #file_name = "dolfin_fine.xml"
+    #file_name = "dolfin_coarse.xml"
+    file_name = "dolfin_fine.xml"
     #file_name = "pinch.xml" 
     mesh_obj = Mesh( "meshes/" + file_name )
 else:
     file_name = "square"
     mesh_obj = UnitSquareMesh( 9, 9)
 
-kappa = 11.23
-dim = 2
-nu = 1
-container  = parameters.Container( mesh_obj, kappa, dim, nu )
-
-
-# All the pbjects used in the variational formulation below
-normal = container.normal 
-u      = container.u
-v      = container.v
-tmp    = Function( container.V )
-f      = Constant( 0.0 )
-
-# Homogeneous Robin ######################################
-hom_beta   = parameters.Robin( container, param = "hom_beta" )
-
-a = inner(grad(u), grad(v))*dx + kappa*kappa*u*v*dx + inner( hom_beta, normal )*u*v*ds
-A = assemble(a)
-L = f*v*dx
-b = assemble(L)
-helper.apply_sources( file_name, container, b )
-
-sol_hom_rob = Function( container.V )
-solve( A, tmp.vector(), b )
-solve( A, sol_hom_rob.vector(), assemble(tmp*v*dx) )
-hom_rob_var = helper.get_var( A, container )
-
-# Inhomogeneous Robin ####################################
-inhom_beta = parameters.Robin( container, param = "inhom_beta" )
-g          = parameters.Robin( container, param = "g"          )
-
-a = inner(grad(u), grad(v))*dx + kappa*kappa*u*v*dx + inner( inhom_beta, normal )*u*v*ds
-A = assemble(a)
-L = f*v*dx + inner( g, normal )*v*ds
-b = assemble(L)
-helper.apply_sources( file_name, container, b )
-
-sol_inhom_rob = Function( container.V )
-solve( A, tmp.vector(), b )
-solve( A, sol_inhom_rob.vector(), assemble(tmp*v*dx) )
-
-
-# Neumann ######################################
-a = inner(grad(u), grad(v))*dx + kappa*kappa*u*v*dx
-A = assemble(a)
-L = f*v*dx
-b = assemble(L)
-helper.apply_sources( file_name, container, b )
-
-sol_neumann = Function( container.V )
-solve( A, tmp.vector(), b )
-solve( A, sol_neumann.vector(), assemble(tmp*v*dx) )
-neumann_var = helper.get_var( A, container )
+# Global variables. Everybody uses them.
+kappa = 11. # Killing rate
+dim = 2 # dimentia
+k = 10000 # number of iterations for ptwise var estimate
 
 
 
-
-
-
-# All the pbjects used in the variational formulation below
+# Improper Homogeneous Robin ######################################
 nu = 0
 container = parameters.Container( mesh_obj, kappa, dim, nu )
 normal = container.normal 
@@ -86,8 +34,6 @@ v      = container.v
 tmp    = Function( container.V )
 f      = Constant( 0.0 )
 
-
-# Improper Homogeneous Robin ######################################
 imp_beta   = parameters.Robin( container, param = "hom_beta" )
 
 a = inner(grad(u), grad(v))*dx + kappa*kappa*u*v*dx + inner( imp_beta, normal )*u*v*ds
@@ -99,8 +45,48 @@ helper.apply_sources( file_name, container, b )
 sol_imp_rob = Function( container.V )
 solve( A, tmp.vector(), b )
 solve( A, sol_imp_rob.vector(), assemble(tmp*v*dx) )
-imp_rob_var = helper.get_var( A, container )
+imp_rob_var = helper.get_var( A, container, k )
 
+
+# Homogeneous Robin ######################################
+nu = 1
+container  = parameters.Container( mesh_obj, kappa, dim, nu )
+#ran = [ 0.0, 0.004 ]
+
+normal = container.normal 
+u      = container.u
+v      = container.v
+tmp    = Function( container.V )
+f      = Constant( 0.0 )
+
+hom_beta   = parameters.Robin( container, param = "hom_beta" )
+
+a = inner(grad(u), grad(v))*dx + kappa*kappa*u*v*dx + inner( hom_beta, normal )*u*v*ds
+A = assemble(a)
+L = f*v*dx
+b = assemble(L)
+helper.apply_sources( file_name, container, b )
+
+sol_hom_rob = Function( container.V )
+solve( A, tmp.vector(), b )
+solve( A, sol_hom_rob.vector(), assemble(tmp*v*dx) )
+hom_rob_var = helper.get_var( A, container, k )
+
+
+# Homogeneous Neumann ######################################
+a = inner(grad(u), grad(v))*dx + kappa*kappa*u*v*dx
+A = assemble(a)
+L = f*v*dx
+b = assemble(L)
+helper.apply_sources( file_name, container, b )
+
+sol_neumann = Function( container.V )
+solve( A, tmp.vector(), b )
+solve( A, sol_neumann.vector(), assemble(tmp*v*dx) )
+neumann_var = helper.get_var( A, container, k )
+
+
+# Fundamental solution
 fundamental = hom_beta.generate( "mat12" )
 fundamental.x[0] = 0.45
 fundamental.x[1] = 0.65
@@ -108,7 +94,81 @@ tmp.interpolate( fundamental )
 tmp.vector()[:] = -tmp.vector()[:]
 
 
-# # difference
+plot( hom_rob_var, 
+      title = "Homogeneous Robin Variance",    
+      mode ="color",
+      #range_min = ran[0],
+      #range_max = ran[1]
+  ).write_png( "../docs/homogeneous_robin_variance" )
+
+plot( neumann_var,
+      title = "Neumann Variance", 
+      mode ="color",
+      #range_min = ran[0],
+      #range_max = ran[1]
+  ).write_png( "../docs/homogeneous_neumann_variance" )
+
+plot( imp_rob_var,
+      title = "Improper Robin Variance",
+      mode ="color",
+      #range_min = ran[0],
+      #range_max = ran[1]
+  ).write_png( "../docs/improper_robin_variance" )
+
+plot( tmp,
+      title = "Fundamental Solution",
+      mode ="color",
+      #range_min = ran[0],
+      #range_max = container.factor
+  ).write_png( "../docs/fundamental_solution" )
+
+plot( sol_hom_rob, 
+      title = "Homogeneous Robin Greens Function",
+      mode = "color",
+      #range_min = ran[0],
+      #range_max = ran[1]
+  ).write_png( "../docs/homogeneous_robin_green" )
+
+plot( sol_imp_rob, 
+      title = "Improper Greens Function",
+      mode ="color" ,
+      #range_min = ran[0],
+      #range_max = ran[1]
+  ).write_png( "../docs/improper_robin_green" )
+
+plot( sol_neumann,
+      title = "Neumann Greens Function",
+      mode ="color",
+      #range_min = ran[0],
+      #range_max = ran[1]
+  ).write_png( "../docs/homogeneous_neumann_green" )
+
+#interactive()
+
+
+
+# # Inhomogeneous Robin ####################################
+# inhom_beta = parameters.Robin( container, param = "inhom_beta" )
+# g          = parameters.Robin( container, param = "g"          )
+
+# a = inner(grad(u), grad(v))*dx + kappa*kappa*u*v*dx + inner( inhom_beta, normal )*u*v*ds
+# A = assemble(a)
+# L = f*v*dx + inner( g, normal )*v*ds
+# b = assemble(L)
+# helper.apply_sources( file_name, container, b )
+
+# sol_inhom_rob = Function( container.V )
+# solve( A, tmp.vector(), b )
+# solve( A, sol_inhom_rob.vector(), assemble(tmp*v*dx) )
+# plot( sol_inhom_rob,
+#       title = "Inhomogeneous Robin Greens Function",
+#       mode ="color",
+#       range_min = ran[0],
+#       range_max = ran[1]
+#   ).write_png( "../docs/inhomogeneous_robin_green" )
+
+
+# Saving and plotting and boriiiinnnngggg....
 # fi = File( 'vis/fundamental.pvd' )
 # fi << tmp 
 # fi = File( 'vis/homogeneous.pvd' )
@@ -120,24 +180,9 @@ tmp.vector()[:] = -tmp.vector()[:]
 # fi = File( 'vis/neumann.pvd' )
 # fi << sol_neumann
 
-plot( hom_rob_var,   title = "Homogeneous Robin Variance"           ).write_pdf( "vis/homogeneous_robin_variance"   )
-plot( neumann_var,   title = "Neumann Variance"                     ).write_pdf( "vis/neumann_variance"       )
-plot( imp_rob_var,   title = "Improper Robin Variance"              ).write_pdf( "vis/improper_robin_variance"   )
-plot( tmp,           title = "Fundamental Solution"                 ).write_pdf( "vis/fundamental_solution"   )
-plot( sol_hom_rob,   title = "Homogeneous Robin Greens Function"    ).write_pdf( "vis/homogeneous_robinn_green"       )
-plot( sol_inhom_rob, title = "Inhomogeneous Robin Greens Function"  ).write_pdf( "vis/inhomogeneous_robin_green"     )
-plot( sol_imp_rob,   title = "Improper Greens Function"             ).write_pdf( "vis/improper_green"       )
-plot( sol_neumann,   title = "Neumann Greens Function"              ).write_pdf( "vis/neumann_green"       )
-
-# plot( sol_hom_rob - sol_inhom_rob,   title = "homogeneous - inhomogeneous"   ) 
-# plot( sol_hom_rob - sol_imp_rob,     title = "homogeneous - improper"        )
-# plot( sol_inhom_rob - sol_imp_rob,   title = "inhomogeneous - improper"      )
-
-# The minus sign is already built into the expression!!!
-# plot( sol_hom_rob - tmp,   title = "homogeneous - fundamental"   ) 
-# plot( sol_imp_rob - tmp,   title = "improper - fundamental"        )
-# plot( sol_inhom_rob - tmp, title = "inhomogeneous - fundamental"      )
-# plot( sol_neumann - tmp, title = "inhomogeneous - fundamental"      )
-
-interactive()
-
+# fi = File( 'vis/homogeneous_robin_var.pvd' )
+# fi << hom_rob_var
+# fi = File( 'vis/improper_robin_var.pvd' )
+# fi << imp_rob_var
+# fi = File( 'vis/neumann_var.pvd' )
+# fi << neumann_var
