11f89448ffc27ec2442cc02130f909a50edb158d
Yair Daon <yd546@nyu.edu>
2016-02-23 16:51:46 -0500

    save plots to correct location

--- 2D/robin.py
+++ 2D/robin.py
@@ -9,19 +9,20 @@ import scipy as sp
 
 # Choose a mesh
 if True:
-    file_name = "lshape.xml"
-    #file_name = "dolfin_coarse.xml"
-    file_name = "dolfin_fine.xml"
-    #file_name = "pinch.xml" 
+    mesh_name = "lshape"
+    mesh_name = "dolfin_coarse"
+    #mesh_name = "dolfin_fine"
+    #mesh_name = "pinch" 
+    file_name = mesh_name + ".xml"
     mesh_obj = Mesh( "meshes/" + file_name )
 else:
-    file_name = "square"
+    mesh_name = "square"
     mesh_obj = UnitSquareMesh( 9, 9)
 
 # Global variables. Everybody uses them.
 kappa = 11. # Killing rate
 dim = 2 # dimentia
-k = 10000 # number of iterations for ptwise var estimate
+k = 2 # number of iterations for ptwise var estimate
 
 
 
@@ -98,91 +99,50 @@ plot( hom_rob_var,
       title = "Homogeneous Robin Variance",    
       mode ="color",
       #range_min = ran[0],
-      #range_max = ran[1]
-  ).write_png( "../docs/homogeneous_robin_variance" )
+      #range_max = ran[1],
+  ).write_png( "../../PriorCov/" + mesh_name + "homogeneous_robin_variance" )
 
 plot( neumann_var,
       title = "Neumann Variance", 
       mode ="color",
       #range_min = ran[0],
       #range_max = ran[1]
-  ).write_png( "../docs/homogeneous_neumann_variance" )
+  ).write_png( "../../PriorCov/" + mesh_name + "homogeneous_neumann_variance" )
 
 plot( imp_rob_var,
       title = "Improper Robin Variance",
       mode ="color",
       #range_min = ran[0],
       #range_max = ran[1]
-  ).write_png( "../docs/improper_robin_variance" )
+  ).write_png( "../../PriorCov/" + mesh_name + "improper_robin_variance" )
 
 plot( tmp,
       title = "Fundamental Solution",
       mode ="color",
       #range_min = ran[0],
       #range_max = container.factor
-  ).write_png( "../docs/fundamental_solution" )
+  ).write_png( "../../PriorCov/" + mesh_name + "fundamental_solution" )
 
 plot( sol_hom_rob, 
       title = "Homogeneous Robin Greens Function",
       mode = "color",
       #range_min = ran[0],
       #range_max = ran[1]
-  ).write_png( "../docs/homogeneous_robin_green" )
+  ).write_png( "../../PriorCov/" + mesh_name + "homogeneous_robin_green" )
 
 plot( sol_imp_rob, 
       title = "Improper Greens Function",
       mode ="color" ,
       #range_min = ran[0],
       #range_max = ran[1]
-  ).write_png( "../docs/improper_robin_green" )
+  ).write_png( "../../PriorCov/" + mesh_name + "improper_robin_green" )
 
 plot( sol_neumann,
       title = "Neumann Greens Function",
       mode ="color",
       #range_min = ran[0],
       #range_max = ran[1]
-  ).write_png( "../docs/homogeneous_neumann_green" )
+  ).write_png( "../../PriorCov/" + mesh_name + "homogeneous_neumann_green" )
 
-#interactive()
+interactive()
 
-
-
-# # Inhomogeneous Robin ####################################
-# inhom_beta = parameters.Robin( container, param = "inhom_beta" )
-# g          = parameters.Robin( container, param = "g"          )
-
-# a = inner(grad(u), grad(v))*dx + kappa*kappa*u*v*dx + inner( inhom_beta, normal )*u*v*ds
-# A = assemble(a)
-# L = f*v*dx + inner( g, normal )*v*ds
-# b = assemble(L)
-# helper.apply_sources( file_name, container, b )
-
-# sol_inhom_rob = Function( container.V )
-# solve( A, tmp.vector(), b )
-# solve( A, sol_inhom_rob.vector(), assemble(tmp*v*dx) )
-# plot( sol_inhom_rob,
-#       title = "Inhomogeneous Robin Greens Function",
-#       mode ="color",
-#       range_min = ran[0],
-#       range_max = ran[1]
-#   ).write_png( "../docs/inhomogeneous_robin_green" )
-
-
-# Saving and plotting and boriiiinnnngggg....
-# fi = File( 'vis/fundamental.pvd' )
-# fi << tmp 
-# fi = File( 'vis/homogeneous.pvd' )
-# fi << sol_hom_rob
-# fi = File( 'vis/inhomogeneous.pvd' )
-# fi << sol_inhom_rob
-# fi = File( 'vis/improper.pvd' )
-# fi << sol_imp_rob
-# fi = File( 'vis/neumann.pvd' )
-# fi << sol_neumann
-
-# fi = File( 'vis/homogeneous_robin_var.pvd' )
-# fi << hom_rob_var
-# fi = File( 'vis/improper_robin_var.pvd' )
-# fi << imp_rob_var
-# fi = File( 'vis/neumann_var.pvd' )
-# fi << neumann_var
